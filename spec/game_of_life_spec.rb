75119caaa83f312c1f0729d4169a8ba6ba32be4f
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-15 10:23:21 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -16,6 +16,14 @@ describe "game_of_life" do
     it "is alive" do
       Cell.new(true).should be_alive
     end
+    
+    context "with less than 2 neighbour" do
+      it "dies when it evolves" do
+        cell = Cell.new(true)
+        cell.evolve(0)
+        cell.should_not be_alive
+      end      
+    end
   end
   
   describe "a dead", Cell do
