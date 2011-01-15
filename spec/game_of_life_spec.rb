0862e54bf5355c42026ec2706c39aaa4b1cccdf0
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-15 12:17:51 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 require 'game_of_life'
 
-describe "game_of_life" do
-  it "does something" do
-    fail "needs some specs"
+describe "GameOfLife" do
+  let(:game) { Game.new }
+  
+  it "has all empty cells" do
+    game.should be_over
   end
 end
