3a8197a9dcef1e6be08837dd07fd47731afa7edc
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-15 10:16:04 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
 require 'game_of_life'
 
+class Cell
+  def alive?
+    true
+  end
+end
+
 describe "game_of_life" do
-  describe "an alive cell" do
+  describe "an alive", Cell do
     it "is alive" do
       Cell.new(true).should be_alive
     end
   end
-  describe "a cell with fewer than 2 neighbours" do
-    it "dies" do
-      pending      
-      cell.evolve
-      cell.should be_alive
-    end
-  end
-end
-
-
-class Cell
-  def alive?
-    true
+  
+  describe "a dead", Cell do
+    
   end
 end
