4495a113ae57399317192f7a660d7d4a6856b7e9
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-15 11:15:23 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 require 'game_of_life'
 
-describe "game_of_life" do
-  it "does something" do
-    fail "needs some specs"
+class Grid
+end
+
+describe "A 1x1 Grid" do
+  it "has width of 1" do
+    Grid.new(1, 1).width.should eq 1
   end
 end
