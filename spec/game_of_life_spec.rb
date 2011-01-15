f7555a814dc84b171eb87169eab547814644e176
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-15 10:12:24 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 require 'game_of_life'
 
 describe "game_of_life" do
-  it "does something" do
-    fail "needs some specs"
+  describe "a cell with fewer than 2 neighbours" do
+    it "dies" do
+      pending      
+      cell.evolve
+      cell.should be_alive
+    end
   end
 end
