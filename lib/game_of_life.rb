a3ba90dc4082ad6197b6d7f0cc2f3469bd02b423
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-16 21:19:49 +0000

    Green

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,3 +1,5 @@
+require_relative 'game_of_life/cell'
+
 class Game
   def initialize(grid)
     @grid = grid
@@ -102,33 +104,5 @@ class Grid
   
   def collect_cell(collection, x, y)
     collection << @cells[y][x] if valid_point?(x, y)
-  end
-end
-
-class Cell
-  def initialize
-    @alive = false
-  end
-  
-  def evolve(neighbours)
-    @alive = evolution_function[neighbours.count { |neighbour| neighbour.alive? } ]
-  end
-    
-  def become_alive
-    @alive = true
-  end
-  
-  def alive?
-    @alive
-  end
-  
-  private
-  
-  def evolution_function
-    if @alive
-      ->(neighbours) { neighbours == 2 || neighbours == 3 }
-    else
-      ->(neighbours) { neighbours == 3 }
-    end
   end
 end
