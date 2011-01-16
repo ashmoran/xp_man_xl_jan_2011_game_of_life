13ca24c10bea97b75392180dad887acd6142ca31
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-16 20:25:39 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -58,7 +58,7 @@ class Grid
     cell_neighbours << @cells[y - 1][x - 1] if x - 1 >= 0 && y - 1 >= 0
     
     # Top-right
-    cell_neighbours << @cells[y + 1][x - 1] if x - 1 >= 0
+    cell_neighbours << @cells[y + 1][x - 1] if y + 1 < height && x - 1 >= 0
     
     cell_neighbours.reject { |cell_neighbour| cell_neighbour.nil? }
   end
