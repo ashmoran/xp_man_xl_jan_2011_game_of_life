7ba173fb29fc1fc72578c9dd02336ac035c5726c
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-16 21:05:05 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -56,14 +56,14 @@ class Grid
   
   def neighbour_points(x, y)
     [
-      [x + 1, y],
-      [x, y + 1],
-      [x + 1, y + 1],
-      [x - 1, y + 1],
-      [x + 1, y - 1],
+      [x - 1, y - 1],
       [x - 1, y],
+      [x - 1, y + 1],
       [x, y - 1],
-      [x - 1, y - 1]
+      [x, y + 1],
+      [x + 1, y - 1],
+      [x + 1, y],
+      [x + 1, y + 1]
     ]
   end
   
