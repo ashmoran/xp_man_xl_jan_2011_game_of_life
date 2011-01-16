4ca1e6a807d6679e627c8fb2d2e3faacc3bc37ba
Ashley Moran <ashley.moran@patchspace.co.uk>
2011-01-16 19:02:43 +0000

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -57,7 +57,7 @@ class Grid
     cell_neighbours << @cells[y - 1][x] rescue nil
     cell_neighbours << @cells[y - 1][x - 1] rescue nil
     
-    cell_neighbours
+    cell_neighbours.reject { |cell_neighbour| cell_neighbour.nil? }
   end
   
   def make_alive(x, y)
